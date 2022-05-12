5f2f3b33a655bc7545c9ab02fb6cc4d113f4ad3b
Ali İhsan İBİLEME <aliihsan@anadolu.edu.tr>
2022-05-12 14:43:59 +0300

    participant kısmında event bilgileri çekildi.

--- graphql/odev01/index.js
+++ graphql/odev01/index.js
@@ -70,6 +70,7 @@ const resolvers = {
     },
     Participant: {
         user: (parent) => users.find((user) => user.id == parent.user_id),
+        event: (parent) => events.find((event) => event.id == parent.event_id)
     }
 }
 
